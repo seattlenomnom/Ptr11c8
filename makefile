2be81add0aa8a9ac1fe88f801a7f246f402e4ff8
seattlenomnom <mark.crapser@gmail.com>
2023-06-07 23:05:18 -0700

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# Makefile for Ptr11c8 project
+#
+ptr11c8: ptr11c8.c
+	gcc -g ptr11c8.c -o ./Build/ptr11c8
---
+++ ptr11c8.c
@@ -0,0 +1,25 @@
+/* ptr11c8.c the source file for Ptr11c8 project
+ *
+ *
+*/
+
+#include <stdio.h>
+
+void test(int *int_pointer);
+
+int main(void){
+
+    int i = 50, *p = &i;
+
+    printf("Before the call to test i = %i\n", i);
+
+    test(p);
+
+    printf("After the call to test, i = %i\n", i);
+
+    return(0);
+}
+
+void text(int *int_pointer){
+    *int_pointer = 100;
+}
